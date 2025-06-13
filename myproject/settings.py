e39e0d57e516c4699ffe55a17840ab27d203eec6
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-06-13 17:15:21 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# عشان Render يعرف يشتغل كويس مع static
+if os.getenv('RENDER'):
+    DEBUG = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
